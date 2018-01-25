1d85d7f80f1cf7d358eccad50a34e7c8e67590f8
ritik1991998 <ritikkne@gmail.com>
2018-01-25 13:46:17 +0530

    sample data filled

--- app/src/main/java/com/example/android/passon/PostAdapter.java
+++ app/src/main/java/com/example/android/passon/PostAdapter.java
@@ -1,9 +1,13 @@
 package com.example.android.passon;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,10 +19,20 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
     private ArrayList<Post> Posts;
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+         ImageView bookPic;
+         TextView bookName,filter1,filter2;
+         CheckBox favouritePost;
+
 
 
         private ViewHolder(View view) {
             super(view);
+            bookPic=(ImageView)view.findViewById(R.id.bookPic);
+            bookName=(TextView)view.findViewById(R.id.bookName);
+            filter1=(TextView)view.findViewById(R.id.filter1);
+            filter2=(TextView)view.findViewById(R.id.filter2);
+            favouritePost = (CheckBox)view.findViewById(R.id.favorite);
+
         }
     }
     public PostAdapter(ArrayList<Post> posts) {
@@ -30,12 +44,18 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.post_feed, parent, false);
 
-        ViewHolder vh = new ViewHolder(v);
-        return  vh;
+        return new ViewHolder(v);
     }
         // create a new view
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
+        final Post post = Posts.get(position);
+        Log.i("point Po53","adapter");
+        holder.filter1.setText(post.getFilter1());
+        holder.filter2.setText(post.getFilter2());
+        holder.bookName.setText(post.getBookName());
+        holder.bookPic.setImageResource(R.drawable.pic);
+        holder.favouritePost.setChecked(true);
 
     }
     @Override
