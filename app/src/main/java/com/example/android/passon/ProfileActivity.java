ff49cac0732f538632b9868a7719f749da173cfa
ritik1991998 <ritikkne@gmail.com>
2018-02-21 20:55:14 +0530

    incomplete chathead

--- app/src/main/java/com/example/android/passon/ProfileActivity.java
+++ app/src/main/java/com/example/android/passon/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.passon;
 
 import android.content.Intent;
+import android.graphics.Movie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -20,11 +21,16 @@ import android.widget.TextView;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.makeramen.roundedimageview.RoundedTransformationBuilder;
 import com.squareup.picasso.Picasso;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+import static com.example.android.passon.Main2Activity.mUserDatabaseReference;
 import static com.example.android.passon.Main2Activity.mUserId;
 
 //import static com.example.android.passon.MainActivity.mUserId;
@@ -34,8 +40,9 @@ public class ProfileActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     public static RecyclerView.Adapter mAdapter;
     public static ChildEventListener mChildEventListenerProfile, mChildEventListenerProfileTest;
-
-    ArrayList<ChatHead> chats;
+    public static DatabaseReference mChildUser;
+    ArrayList<ChatHead[]> chats;
+    ArrayList<String> chatsString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +55,11 @@ public class ProfileActivity extends AppCompatActivity {
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 //        getSupportActionBar().setTitle("Profile");
 
+//        mChildUser= Main2Activity.mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).
+
         mRecyclerView = (RecyclerView) findViewById(R.id.chatHead);
         chats = new ArrayList<>();
+        chatsString = new ArrayList<String>();
         mAdapter = new ChatNameAdapter(chats);
 //        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
 
@@ -100,10 +110,11 @@ public class ProfileActivity extends AppCompatActivity {
                     Log.i("onchildadded", "standpoint pr100");
 
                     //attached to all added child(all past and future child)
-                    ChatHead chatHead = dataSnapshot.getValue(ChatHead.class);
-                    chats.add(chatHead);
-                    Log.i("onitemadded", "standpoint pr105");
-
+//                    ChatHead chatHead = dataSnapshot.getValue(ChatHead.class);
+//                    chats.add(chatHead);
+                    for (DataSnapshot child : dataSnapshot.getChildren()) {
+                        Log.i(child.getValue().toString(), "standpoint pr106");
+                    }
                     mAdapter.notifyDataSetChanged();
                 }
 
@@ -128,11 +139,65 @@ public class ProfileActivity extends AppCompatActivity {
                 }
 
             };
-            Main2Activity.mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).getRef().child("connectionRequestUsers").addChildEventListener(mChildEventListenerProfile);
+//            mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).orderByChild("phoneNo").addChildEventListener(mChildEventListenerProfile);
+//                    .addChildEventListener(mChildEventListenerProfile);
+//            Main2Activity.mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).getRef().child("phoneNo").addChildEventListener(mChildEventListenerProfile);
+//            Main2Activity.mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).getRef().child("connectionRequestUsers").addChildEventListener(mChildEventListenerProfile);
 //            Main2Activity.mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).getRef().child("connectionRequestUsers").addChildEventListener(mChildEventListenerProfile);
 //            mPostDatabaseReference = mfirebaseDatabase.getReference().child("post1");
+            Log.i("dataSnapshot.toString()", "point 145");
 
-//            Query query = mUserDatabaseReference.orderByChild("userId").equalTo(posteruid);
+            Query query = mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId);
+            query.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    Log.i("toString()", "point 151");
+                    UserInfo userInfo = dataSnapshot.getValue(UserInfo.class);
+                    chats.add(new ArrayList<ChatHead>(userInfo.getConnectionRequestUsers()));
+                    ArrayList<ChatHead> ram1=(ArrayList<ChatHead>)userInfo.getConnectionRequestUsers();
+
+                    Log.i("point 154",userInfo.getPhoneNo()+"");
+                    Log.i("point 161",ram1.get(0).getUsername());
+
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+//                    new ValueEventListener() {
+//                @Override
+//                public void onDataChange(DataSnapshot dataSnapshot) {
+//                    for (DataSnapshot movieSnapshot : dataSnapshot.getChildren()) {
+////                        Movie movie = dataSnapshot.getValue(Movie.class);
+//                        Log.i(dataSnapshot.toString(),"point 152");
+//                        Log.i(movieSnapshot.toString(),"point 153");
+//
+//                    }
+//                }
+//
+//                @Override
+//                public void onCancelled(DatabaseError databaseError) {
+//
+//                }
+//            });
 //            query.addListenerForSingleValueEvent(new ValueEventListener() {
 //                @Override
 //                public void onDataChange(DataSnapshot dataSnapshot) {
@@ -168,30 +233,31 @@ public class ProfileActivity extends AppCompatActivity {
 //                        }                    }
                         }
                     }
-                            @Override
-                            public void onChildChanged (DataSnapshot dataSnapshot, String s){
-                                // changed content of a child
-                            }
 
-                            @Override
-                            public void onChildRemoved (DataSnapshot dataSnapshot){
-                                // child deleted
-                            }
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                        // changed content of a child
+                    }
 
-                            @Override
-                            public void onChildMoved (DataSnapshot dataSnapshot, String s){
-                                //moved position of a child
-                            }
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                        // child deleted
+                    }
 
-                            @Override
-                            public void onCancelled (DatabaseError databaseError){
-                                // error or permission denied
-                            }
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                        //moved position of a child
+                    }
 
-                        } ;
-                        Main2Activity.mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).addChildEventListener(mChildEventListenerProfileTest);
-                        if (mChildEventListenerProfileTest != null) {
-                            Log.i("mChildEventLisrPro add", "standpoint pr183");
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // error or permission denied
+                    }
+
+                };
+                mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).getRef().child("connectionRequestUsers").addChildEventListener(mChildEventListenerProfileTest);
+                if (mChildEventListenerProfileTest != null) {
+                    Log.i("mChildEventLisrPro add", "standpoint pr183");
 //                Query query = mUserDatabaseReference.orderByChild("userId").equalTo(posteruid);
 //                query.addListenerForSingleValueEvent(new ValueEventListener() {
 //                    @Override
@@ -199,32 +265,34 @@ public class ProfileActivity extends AppCompatActivity {
 //                        for (DataSnapshot child : dataSnapshot.getChildren()) {
 //                            child.getRef().child("connectionRequestUsers").push().setValue(requesterUid);
 //                        }
-                        }
-                    }
                 }
             }
-            @Override
-            protected void onPause () {
-                super.onPause();
-                System.out.println("standpoint pr112");
-                if (mChildEventListenerProfile != null) {
-                    Log.i(mChildEventListenerProfile.toString(), "standpoint pr114");
-                    Main2Activity.mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).getRef().child("connectionRequestUsers").removeEventListener(mChildEventListenerProfile);
-                    mChildEventListenerProfile = null;
-                }
-                    if (mChildEventListenerProfileTest != null) {
-                        Log.i(mChildEventListenerProfileTest.toString(), "standpoint pr114");
-//                        Main2Activity.mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).getRef().child("connectionRequestUsers").removeEventListener(mChildEventListenerProfile);
-                        Main2Activity.mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).removeEventListener(mChildEventListenerProfileTest);
-                        mChildEventListenerProfileTest = null;
-                    }
-                chats.clear();
-                mAdapter.notifyItemRangeRemoved(0, mAdapter.getItemCount());
-            }
-            @Override
-            public boolean onSupportNavigateUp () {
-                finish();
-                return true;
-            }
-
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("standpoint pr112");
+        if (mChildEventListenerProfile != null) {
+            Log.i(mChildEventListenerProfile.toString(), "standpoint pr114");
+            mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).getRef().child("connectionRequestUsers").removeEventListener(mChildEventListenerProfile);
+            mChildEventListenerProfile = null;
+        }
+        if (mChildEventListenerProfileTest != null) {
+            Log.i(mChildEventListenerProfileTest.toString(), "standpoint pr114");
+//                        Main2Activity.mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).getRef().child("connectionRequestUsers").removeEventListener(mChildEventListenerProfile);
+            mUserDatabaseReference.orderByChild("userId").equalTo(Main2Activity.mUserId).removeEventListener(mChildEventListenerProfileTest);
+            mChildEventListenerProfileTest = null;
+        }
+        chats.clear();
+        mAdapter.notifyItemRangeRemoved(0, mAdapter.getItemCount());
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+
+}
