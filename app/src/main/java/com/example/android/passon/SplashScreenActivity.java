a7d1375d7ab02d8d441957c7cb5a1865edfc1c28
ritik1991998 <ritikkne@gmail.com>
2018-04-11 01:14:20 +0530

    splash screen method updated

--- app/src/main/java/com/example/android/passon/SplashScreenActivity.java
+++ app/src/main/java/com/example/android/passon/SplashScreenActivity.java
@@ -1,15 +1,8 @@
 package com.example.android.passon;
 
-import android.annotation.SuppressLint;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -17,23 +10,14 @@ import android.view.View;
  */
 public class SplashScreenActivity extends AppCompatActivity {
 
-
-    private static final long SPLASH_TIME_OUT = 100;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+//        setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(SplashScreenActivity.this, Main2Activity.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
-
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
